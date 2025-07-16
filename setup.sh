cca17230234bcb2d2b89aa72b8da69303e921c93
Marcos Camargo <92488295+Marcos-Camargo@users.noreply.github.com>
2025-07-16 20:38:40 -0300

    Address shellcheck warnings

--- setup.sh
+++ setup.sh
@@ -18,11 +18,12 @@ echo
 if [ ! -d "src/public" ]; then
     echo "Clonando repositório..."
     mkdir -p src
-    cd src
-    git clone "$REPO"
-    mv Fase1 public
-    rm -rf Fase1
-    cd ..
+    (
+        cd src || exit
+        git clone "$REPO"
+        mv Fase1 public
+        rm -rf Fase1
+    )
     echo "Copiando arquivos da pasta files/ para src/public/"
     cp -a files/. src/public/
 fi
@@ -37,9 +38,9 @@ chmod +x \
 find src/public -type f -name "*.sh" -exec chmod +x {} +
 
 echo
-echo "\xF0\x9F\xA7\xB9 Limpando arquivos temporários..."
+printf '\xF0\x9F\xA7\xB9 Limpando arquivos temporários...\n'
 find . -type f -name "*Zone.Identifier*" -exec rm -f {} +
 
 echo
-echo "\xF0\x9F\x9A\x80 Iniciando containers..."
+printf '\xF0\x9F\x9A\x80 Iniciando containers...\n'
 docker-compose up -d
